0134317393b3830fb59d477b9387598ab6c9ba63
Savio Caetano <saviocardoso27@gmail.com>
2023-07-28 18:45:19 -0300

    History Check-ins Use Case and tests

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -5,7 +5,7 @@ import dayjs from "dayjs"
 
 export class InMemoryCheckInsRepository implements CheckInsRepository{
     public items: CheckIn[] = []
-    
+
     async findByUserIdOnDate(userId: string, date: Date) {
         const startOfTheDay = dayjs(date).startOf('date')
         const endOfTheDay = dayjs(date).endOf('date')
@@ -22,6 +22,12 @@ export class InMemoryCheckInsRepository implements CheckInsRepository{
         }
         
         return checkInOnSameDate
+    }
+
+    async findManyByUserId(userId: string, page: number) {
+        return this.items
+            .filter((item) => item.user_Id == userId)
+            .slice((page - 1) * 20, page * 20)
     }
     
     async create(data: Prisma.CheckInUncheckedCreateInput) {
